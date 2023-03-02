8d2b1973ecfc3aae12308561902bc7412d0f67b6
Vita <malavita29@mail.ru>
2023-03-02 20:24:17 +0300

    search gif

--- app/src/main/java/com/vita_zaebymba/gifsearchapp/GiphyClient.kt
+++ app/src/main/java/com/vita_zaebymba/gifsearchapp/GiphyClient.kt
@@ -1,7 +1,10 @@
 package com.vita_zaebymba.gifsearchapp
 
+import com.giphy.sdk.core.network.api.Constants.API_KEY
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 
 class GiphyClient {
     private val service = Retrofit.Builder()
@@ -10,7 +13,7 @@ class GiphyClient {
         .build()
         .create(GiphyService::class.java)
 
-    suspend fun getTrendingGifs(): List<Gif> {
+    suspend fun getTrendingGifs(query: String): List<Gif> {
         val response = service.getTrendingGifs()
         return if (response.isSuccessful) {
             response.body()?.data?.map { gif ->
@@ -27,4 +30,8 @@ class GiphyClient {
             emptyList()
         }
     }
+
+
+
+
 }
--- app/src/main/java/com/vita_zaebymba/gifsearchapp/MainActivity.kt
+++ app/src/main/java/com/vita_zaebymba/gifsearchapp/MainActivity.kt
@@ -4,11 +4,13 @@ import android.annotation.SuppressLint
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.widget.EditText
 import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import kotlinx.coroutines.launch
-import java.io.Serializable
 
 class MainActivity : AppCompatActivity() {
     private val giphyClient = GiphyClient()
@@ -28,12 +30,24 @@ class MainActivity : AppCompatActivity() {
         }
         gifList.adapter = adapter
 
-        lifecycleScope.launch {
-            val gifs = giphyClient.getTrendingGifs()
-            adapter.gifs = gifs
-            adapter.notifyDataSetChanged()
-        }
-    }
+        val searchBar: EditText = findViewById(R.id.search_bar)
+        searchBar.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+                // not needed
+            }
 
-    
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                // not needed
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+                lifecycleScope.launch {
+                    val query = s?.toString()?.trim() ?: ""
+                    val gifs = giphyClient.getTrendingGifs(query)
+                    adapter.gifs = gifs
+                    adapter.notifyDataSetChanged()
+                }
+            }
+        })
+    }
 }
