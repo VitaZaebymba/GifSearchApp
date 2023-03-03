92be9dd7681123d3a6214149fd3da623df6f7e5c
Vita <malavita29@mail.ru>
2023-03-03 12:47:52 +0300

    add comments

--- app/src/main/java/com/vita_zaebymba/gifsearchapp/Gif.kt
+++ app/src/main/java/com/vita_zaebymba/gifsearchapp/Gif.kt
@@ -1,6 +1,6 @@
 package com.vita_zaebymba.gifsearchapp
 
-data class Gif(
+data class Gif( //класс для хранения информации о GIF
     val id: String,
     val title: String,
     val url: String,
--- app/src/main/java/com/vita_zaebymba/gifsearchapp/MainActivity.kt
+++ app/src/main/java/com/vita_zaebymba/gifsearchapp/MainActivity.kt
@@ -35,5 +35,5 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    
+
 }
