80018cdeef401c1b1e22ed2bd5a6cdbc97f4a9bf
Vita <malavita29@mail.ru>
2023-03-03 12:57:06 +0300

    add comments

--- app/src/main/java/com/vita_zaebymba/gifsearchapp/GifAdapter.kt
+++ app/src/main/java/com/vita_zaebymba/gifsearchapp/GifAdapter.kt
@@ -7,33 +7,36 @@ import android.widget.ImageView
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 
-class GifAdapter(var gifs: List<Gif>, private val onItemClick: (Gif) -> Unit) : RecyclerView.Adapter<GifAdapter.ViewHolder>() {
+// класс для отображения списка GIF-изображений в RecyclerView
+class GifAdapter(var gifs: List<Gif>, private val onItemClick: (Gif) -> Unit) : RecyclerView.Adapter<GifAdapter.ViewHolder>() { // принимает список gif-изображений и функцию onItemClick, которая будет вызываться при нажатии на элемент списка
 
-    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) { // Внутренний класс ViewHolder используется для создания представлений элементов списка и связывания их с данными
         private val gifImage: ImageView = itemView.findViewById(R.id.gif_image)
 
-        fun bind(gif: Gif) {
+// содержит ссылки на представления элемента списка (в данном случае ImageView) и метод bind(), который связывает данные GIF-изображения с представлениями
+
+        fun bind(gif: Gif) { // использует библиотеку Glide для загрузки предварительного изображения GIF с помощью URL-адреса, указанного в свойстве previewUrl экземпляра класса Gif
             Glide.with(itemView.context)
                 .load(gif.previewUrl)
                 .into(gifImage)
 
-            itemView.setOnClickListener {
+            itemView.setOnClickListener { // Функция устанавливает слушатель нажатий на элементе списка
                 onItemClick(gif)
             }
         }
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder { // Метод onCreateViewHolder создает новый ViewHolder, заполняя его данными из макета элемента списка
         val view = LayoutInflater.from(parent.context)
             .inflate(R.layout.item_gif, parent, false)
         return ViewHolder(view)
     }
 
-    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) { // Метод onBindViewHolder вызывает метод bind() для ViewHolder, передавая ему данные GIF-изображения для связывания с представлением элемента списка
         holder.bind(gifs[position])
     }
 
-    override fun getItemCount(): Int {
+    override fun getItemCount(): Int { // Метод getItemCount возвращает количество элементов в списке GIF-изображений
         return gifs.size
     }
 }
--- app/src/main/java/com/vita_zaebymba/gifsearchapp/GifDetailActivity.kt
+++ app/src/main/java/com/vita_zaebymba/gifsearchapp/GifDetailActivity.kt
@@ -4,9 +4,9 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
 
-class GifDetailActivity : AppCompatActivity() {
+class GifDetailActivity : AppCompatActivity() { // класс для отображения подробной информации о выбранном GIF-изображении
     companion object {
-        const val EXTRA_GIF = "extra_gif"
+        const val EXTRA_GIF = "extra_gif" // константа используется для передачи экземпляра класса Gif между различными компонентами приложения
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,7 +16,7 @@ class GifDetailActivity : AppCompatActivity() {
         val gif: Gif = intent.getParcelableExtra(EXTRA_GIF)!!
 
 
-        val gifTitle: TextView = findViewById(R.id.gif_title)
+        val gifTitle: TextView = findViewById(R.id.gif_title) // текстовому полю gifTitle устанавливается текст заголовка GIF-изображения с помощью метода setText, используя свойство title объекта gif
         gifTitle.text = gif.title
 
 
