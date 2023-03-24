7e555a9112c5f7ad909e2c279a5b7a078c6e12e5
Vita <malavita29@mail.ru>
2023-03-24 14:21:34 +0300

    update code

---
+++ app/src/main/java/com/vita_zaebymba/gifsearchapp/activity/MainActivity.kt
@@ -0,0 +1,57 @@
+package com.vita_zaebymba.gifsearchapp.activity
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.EditText
+import androidx.core.widget.doOnTextChanged
+import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.vita_zaebymba.gifsearchapp.adapter.GifAdapter
+import com.vita_zaebymba.gifsearchapp.GiphyClient
+import com.vita_zaebymba.gifsearchapp.R
+import com.vita_zaebymba.gifsearchapp.coroutine.Coroutine
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
+
+class MainActivity : AppCompatActivity() {
+    private val giphyClient = GiphyClient() // создает экземпляр GiphyClient и использует его для получения популярных GIF-изображений с помощью метода getTrendingGifs()
+
+    private lateinit var editText: EditText
+    private lateinit var gifList: RecyclerView
+
+    @SuppressLint("NotifyDataSetChanged")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        gifList = findViewById(R.id.gif_list) // полученные данные используются для заполнения RecyclerView, который отображает список GIF-изображений
+        editText = findViewById(R.id.search_bar)
+        gifList.layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
+
+        val adapter = GifAdapter(emptyList()) { gif ->
+            val intent = GifDetailActivity.newIntent(this@MainActivity, gif)
+            startActivity(intent)
+        }
+        gifList.adapter = adapter
+
+        editText.doOnTextChanged { text, start, before, count ->
+
+            if (text.toString().isEmpty()) {
+                Coroutine.coroutineTrendingGifs(lifecycleScope, giphyClient, adapter)
+            } else {
+                Coroutine.coroutineSearchGifs(
+                    lifecycleScope,
+                    giphyClient,
+                    adapter,
+                    editText.text.toString()
+                )
+            }
+        }
+
+        Coroutine.coroutineTrendingGifs(lifecycleScope, giphyClient, adapter)
+    }
+
+}
--- app/src/main/java/com/vita_zaebymba/gifsearchapp/data/Gif.kt
+++ app/src/main/java/com/vita_zaebymba/gifsearchapp/data/Gif.kt
@@ -1,4 +1,4 @@
-package com.vita_zaebymba.gifsearchapp
+package com.vita_zaebymba.gifsearchapp.data
 
 data class Gif( //класс для хранения информации о GIF
     val id: String,
@@ -7,4 +7,4 @@ data class Gif( //класс для хранения информации о GIF
     val previewUrl: String,
     val width: Int,
     val height: Int
-)
+) : java.io.Serializable
